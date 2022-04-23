a6089cca65456348577c46d00192b90bf1508cc5
BenFSaks <83617955+BenFSaks@users.noreply.github.com>
2022-04-23 14:24:00 -0400

    Intial Setup

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const path = require('path')
+var app = express()
+//app.set('views', path.join(__dirname, 'public/views'))
+app.use(express.static(path.join(__dirname,'public/views')))
+app.get('/', (req,res) =>{
+    res.render("index.html")
+})
+
+app.listen(3000)
+console.log("http://www.localhost:3000")
