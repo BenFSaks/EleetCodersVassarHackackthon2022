58d328dd9675e5069fe6f4cf2c59b83d82a42b61
Yuvraj Singh <89746017+YuviSingh18@users.noreply.github.com>
2022-04-24 11:59:15 -0400

    data collection

--- app.js
+++ app.js
@@ -16,41 +16,27 @@ app.use(function(req, res, next) {
     next();
   })
 //app.use(express.static(path.join(__dirname,'public/css')))
-// let CITY_NAME 
-// let STATE_NAME 
-// let COUNTRY_NAME
+let CITY_NAME = "Beijing"
+let STATE_NAME = "Vermont"
+let COUNTRY_NAME = "China"
 let YOUR_API_KEYS = ["8dfd441d-056a-47dc-99c5-c963c191a9c2","2e8262db-5b85-41c7-823b-6daef46b84ea","b23a158b-58cb-4889-9464-60cc78ce9cdc","f5a0c748-5f7b-4e84-b306-d7648b37560b","c56c9652-fdfe-4192-98f8-d60ff2452d0e","885aa734-2186-40e5-a9ff-3c29a7b6fd1e","8da51ed9-a5ca-4a8f-ae05-4f54b5aa33df"]
-
-// function setState(state){
-//     STATE_NAME = state
-// }
-// function setCountry(country){
-//     COUNTRY_NAME = country
-// }
 
 var https = require('follow-redirects').https;
 var fs = require('fs');
 
-// var options = {
-//   'method': 'GET',
-//   'hostname': 'api.airvisual.com',
-//   'path': `/v2/cities?state=${STATE_NAME}&country=${COUNTRY_NAME}&key=b23a158b-58cb-4889-9464-60cc78ce9cdc`,
-//   'headers': {
-//   },
-//   'maxRedirects': 20
-// };
+var options = {
+  'method': 'GET',
+  'hostname': 'api.airvisual.com',
+  'path': `/v2/cities?state=${STATE_NAME}&country=USA&key=b23a158b-58cb-4889-9464-60cc78ce9cdc`,
+  'headers': {
+  },
+  'maxRedirects': 20
+};
 
+var cityData = new Set()
+loadAQI()
 //Get Air Quality Index
-function loadAQI(STATE_NAME, COUNTRY_NAME) {
-    let options = {
-        'method': 'GET',
-        'hostname': 'api.airvisual.com',
-        'path': `/v2/cities?state=${STATE_NAME}&country=${COUNTRY_NAME}&key=${YOUR_API_KEYS[Math.floor(Math.random()*7)]}`,
-        'headers': {
-        },
-        'maxRedirects': 20
-      };
-
+function loadAQI(){
     let getNearCitiesCall = new Promise ((resolve, reject) => {
 
         var req = https.request(options, function(res){
@@ -76,26 +62,27 @@ function loadAQI(STATE_NAME, COUNTRY_NAME) {
     })
     getNearCitiesCall.then((response) => {
         let Cities = response
-        getCitiesData(Cities, STATE_NAME, COUNTRY_NAME)
+        getCitiesData(Cities)
     }).catch((error) =>{
         console.log(error)
     })
 
 }
 
-function getCitiesData(cities, STATE_NAME, COUNTRY_NAME) {
+function getCitiesData(cities){
     citiesJson = JSON.parse(cities)
+    console.log(citiesJson)
     cityNames = citiesJson.data
-    for (let i = 0; i < cityNames.length; i++) {
-        getData(cityNames[i].city, YOUR_API_KEYS[Math.floor(Math.random()*7)], STATE_NAME, COUNTRY_NAME)
+    for(let i = 0; i < 14; i++ ) {
+        getData(cityNames[i].city, YOUR_API_KEYS[i%7])
     }
 }
 
-function getData(city, key, STATE_NAME, COUNTRY_NAME) {
+function getData(city, key) {
   var options2 = {
     'method': 'GET',
     'hostname': 'api.airvisual.com',
-    'path': `/v2/city?city=${city}&state=${STATE_NAME}&country=${COUNTRY_NAME}&key=${key}`,
+    'path': `/v2/city?city=${city}&state=${STATE_NAME}&country=USA&key=${key}`,
     'headers': {
     },
     'maxRedirects': 20
@@ -133,12 +120,18 @@ let citiesList = []
 
 function printCityData(data) {
   dataJson = JSON.parse(data)
-  cityData = {}
-  cityData.coords = dataJson.data.location.coordinates
-  cityData.city = dataJson.data.city
-  cityData.pollution = dataJson.data.current.pollution.aqius
-  citiesList.push(cityData)
-  weather = dataJson.data.current.pollution
+  let str = JSON.stringify(dataJson) + "\n"
+  fs.appendFile(`${STATE_NAME}.txt`, str, (err) => {
+
+    // In case of a error throw err.
+    if (err) throw err;
+})
+  // cityData = {}
+  // cityData.coords = dataJson.data.location.coordinates
+  // cityData.city = dataJson.data.city
+  // cityData.pollution = dataJson.data.current.pollution.aqius
+  // citiesList.push(cityData)
+  // weather = dataJson.data.current.pollution
 }
 
 topRanking = []
@@ -163,9 +156,41 @@ topRanking[2] = {
 
 
 
+
+
+app.get('/top-polluted' ,(req, res) =>{
+    res.render("topPolluted.ejs", {
+        name: "Ben",
+        cityList: topRanking
+    })
+})
 app.get('/organizations' ,(req, res) =>{
     res.render("organizations.ejs")
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // Routing
 app.get('/', (req,res) =>{
@@ -184,7 +209,10 @@ app.get('/form', (req,res) =>{
     })
 })
 app.post('/near-by', (req,res)=>{
-    loadAQI(req.body.state, req.body.country)
+    CITY_NAME = req.body.city
+    STATE_NAME = req.body.state
+    COUNTRY_NAME = req.body.country
+    loadAQI()
     res.send(citiesList)
 })
 
