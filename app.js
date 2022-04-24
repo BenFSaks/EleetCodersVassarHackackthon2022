94af646388c10aa09c92a3281a3cada37b6cef13
johnnytejada <johnnyfran20002@gmail.com>
2022-04-24 11:33:32 -0400

    appjs

--- app.js
+++ app.js
@@ -16,26 +16,41 @@ app.use(function(req, res, next) {
     next();
   })
 //app.use(express.static(path.join(__dirname,'public/css')))
-let CITY_NAME = "Beijing"
-let STATE_NAME = "New%20York"
-let COUNTRY_NAME = "China"
+// let CITY_NAME 
+// let STATE_NAME 
+// let COUNTRY_NAME
 let YOUR_API_KEYS = ["8dfd441d-056a-47dc-99c5-c963c191a9c2","2e8262db-5b85-41c7-823b-6daef46b84ea","b23a158b-58cb-4889-9464-60cc78ce9cdc","f5a0c748-5f7b-4e84-b306-d7648b37560b","c56c9652-fdfe-4192-98f8-d60ff2452d0e","885aa734-2186-40e5-a9ff-3c29a7b6fd1e","8da51ed9-a5ca-4a8f-ae05-4f54b5aa33df"]
+
+// function setState(state){
+//     STATE_NAME = state
+// }
+// function setCountry(country){
+//     COUNTRY_NAME = country
+// }
 
 var https = require('follow-redirects').https;
 var fs = require('fs');
 
-var options = {
-  'method': 'GET',
-  'hostname': 'api.airvisual.com',
-  'path': `/v2/cities?state=${STATE_NAME}&country=USA&key=b23a158b-58cb-4889-9464-60cc78ce9cdc`,
-  'headers': {
-  },
-  'maxRedirects': 20
-};
+// var options = {
+//   'method': 'GET',
+//   'hostname': 'api.airvisual.com',
+//   'path': `/v2/cities?state=${STATE_NAME}&country=${COUNTRY_NAME}&key=b23a158b-58cb-4889-9464-60cc78ce9cdc`,
+//   'headers': {
+//   },
+//   'maxRedirects': 20
+// };
 
-loadAQI()
 //Get Air Quality Index
-function loadAQI(){
+function loadAQI(STATE_NAME, COUNTRY_NAME) {
+    let options = {
+        'method': 'GET',
+        'hostname': 'api.airvisual.com',
+        'path': `/v2/cities?state=${STATE_NAME}&country=${COUNTRY_NAME}&key=${YOUR_API_KEYS[Math.floor(Math.random()*7)]}`,
+        'headers': {
+        },
+        'maxRedirects': 20
+      };
+
     let getNearCitiesCall = new Promise ((resolve, reject) => {
 
         var req = https.request(options, function(res){
@@ -61,26 +76,24 @@ function loadAQI(){
     })
     getNearCitiesCall.then((response) => {
         let Cities = response
-        getCitiesData(Cities)
+        getCitiesData(Cities, STATE_NAME, COUNTRY_NAME)
     }).catch((error) =>{
         console.log(error)
     })
 
 }
 
-function getCitiesData(cities){
+function getCitiesData(cities, STATE_NAME, COUNTRY_NAME) {
     citiesJson = JSON.parse(cities)
     cityNames = citiesJson.data
-    for(let i = 0; i < 10; i++ ) {
-        getData(cityNames[i].city, YOUR_API_KEYS[i%7])
-    }
+    getData(cityNames[i].city, YOUR_API_KEYS[Math.floor(Math.random()*7)], STATE_NAME, COUNTRY_NAME)
 }
 
-function getData(city, key) {
+function getData(city, key, STATE_NAME, COUNTRY_NAME) {
   var options2 = {
     'method': 'GET',
     'hostname': 'api.airvisual.com',
-    'path': `/v2/city?city=${city}&state=${STATE_NAME}&country=USA&key=${key}`,
+    'path': `/v2/city?city=${city}&state=${STATE_NAME}&country=${COUNTRY_NAME}&key=${key}`,
     'headers': {
     },
     'maxRedirects': 20
@@ -163,27 +176,6 @@ app.get('/organizations' ,(req, res) =>{
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 // Routing
 app.get('/', (req,res) =>{
     res.render("index.ejs")
@@ -201,10 +193,7 @@ app.get('/form', (req,res) =>{
     })
 })
 app.post('/near-by', (req,res)=>{
-    CITY_NAME = req.body.city
-    STATE_NAME = req.body.state
-    COUNTRY_NAME = req.body.country
-    loadAQI()
+    loadAQI(req.body.state, req.body.country)
     res.send(citiesList)
 })
 
